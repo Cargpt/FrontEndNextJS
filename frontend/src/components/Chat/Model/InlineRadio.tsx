cc46bba9e70c3c8a238af34bee262caf65c88b63
vijay-aivorallp <vijay.rajput@aivorallp.com>
2025-06-23 11:36:38 +0530

    minor changed

--- frontend/src/components/Chat/Model/InlineRadio.tsx
+++ frontend/src/components/Chat/Model/InlineRadio.tsx
@@ -36,26 +36,41 @@ onChange?.(event.target.name, isNaN(Number(newValue)) ?  newValue : Number(newVa
       };
 
 
-  console.log(options)
+  
   return (
-    <FormControl component="fieldset" >
-      <FormLabel component="legend">{label}</FormLabel>
-      <RadioGroup
-        name={label.toLocaleLowerCase().replace(" ", "_")}
-        value={selectedValue?.toString() ?? ''}
-        onChange={handleChange}
-        row
-      >
-        {options.map((option) => (
-          <FormControlLabel
-            key={option}
-            value={option}
-            control={<Radio size="small"/>}
-            label={option}
-          />
-        ))}
-      </RadioGroup>
-    </FormControl>
+<FormControl component="fieldset" fullWidth>
+  <FormLabel component="legend">{label}</FormLabel>
+  <RadioGroup
+    name={label.toLowerCase().replace(/\s+/g, "_")}
+    value={selectedValue?.toString() ?? ""}
+    onChange={handleChange}
+    sx={{
+      display: "flex",
+      flexDirection: {
+        xs: "column", // stack vertically on extra-small screens
+        sm: "row",    // align horizontally from small screens and up
+      },
+      gap: 1, // space between radio buttons
+      flexWrap: "wrap", // allow wrap if too many options
+    }}
+  >
+    {options.map((option) => (
+      <FormControlLabel
+        key={option}
+        value={option}
+        control={<Radio size="small" />}
+        label={option}
+        sx={{
+          minWidth: {
+            xs: "100%",  // each radio takes full width on mobile
+            sm: "auto",  // shrink to content on larger screens
+          },
+        }}
+      />
+    ))}
+  </RadioGroup>
+</FormControl>
+
   );
 };
 
