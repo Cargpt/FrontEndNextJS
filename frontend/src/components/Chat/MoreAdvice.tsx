c1883d3aeeb863713b4042d96c685462c20f24e1
vijay-aivorallp <vijay.rajput@aivorallp.com>
2025-07-24 10:48:47 +0530

    minor changed

--- frontend/src/components/Chat/MoreAdvice.tsx
+++ frontend/src/components/Chat/MoreAdvice.tsx
@@ -27,7 +27,7 @@ const BasicCard:React.FC<BasicCardProps>=({title})=> {
          {title}
         </Typography> */}
         <Typography variant="body2" color="text.secondary">
-          <BrandSelector label={title}  options={["5L-10L", "10L-15L"]} onChnageFilter={()=>{}} />
+          <BrandSelector currentBudget='5-10L' label={title}  budgetTypes={["5L-10L", "10L-15L"]} handleBudgetChange={()=>{}}  />
         </Typography>
       </CardContent>
       <CardActions>
