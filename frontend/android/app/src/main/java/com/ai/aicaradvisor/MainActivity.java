fbe2af7a4bbf64a9c5d4f8a5903693ba815f45cb
vijay-aivorallp <vijay.rajput@aivorallp.com>
2025-08-27 11:03:55 +0530

    removed android/ from git ignore

---
+++ frontend/android/app/src/main/java/com/ai/aicaradvisor/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ai.aicaradvisor;
+import android.os.Bundle; // 👈 THIS IS REQUIRED
+
+import com.getcapacitor.BridgeActivity;
+
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Register plugin
+    registerPlugin(GoogleAuth.class);
+  }
+
+}
+
